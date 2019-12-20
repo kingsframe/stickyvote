2cff0bbeaef50248c6367db02eaf50aaa9539e62
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-12-20 12:40:27 -0800

    no more droids

--- stickyvote.go
+++ stickyvote.go
@@ -10,16 +10,8 @@ var Schema = `
 		query: Query
 	}
 	type Query {
-		droid(id: ID!): Droid!
 		#getTopics(): [Topic!]!
 		getDemoTopic(): Topic!
-	}
-	# An autonomous mechanical character in the Star Wars universe
-	type Droid {
-		# The ID of the droid
-		id: ID!
-		# What others call this droid
-		name: String!
 	}
 	type Topic {
 		left: String!
@@ -31,12 +23,6 @@ var Schema = `
 		RIGHT
 	}	
 `
-
-type droid struct {
-	ID   graphql.ID
-	Name string
-}
-
 type topic struct {
 	ID       graphql.ID
 	Left     string
@@ -44,7 +30,6 @@ type topic struct {
 	HasVoted *string //TODO check if this is correct
 }
 
-var droidData = make(map[graphql.ID]*droid)
 var topicData = make(map[graphql.ID]*topic)
 
 func init() {
@@ -54,14 +39,7 @@ func init() {
 		{ID: "0", Left: "Bernie", Right: "Trump", HasVoted: &CHOICE_LEFT},
 		{ID: "1", Left: "F150", Right: "CyberTruck", HasVoted: &CHOICE_RIGHT},
 	}
-	var droids = []*droid{
-		{ID: "2000", Name: "C-3PO"},
-		{ID: "2001", Name: "R2-D2"},
-	}
-	
-	for _, d := range droids {
-		droidData[d.ID] = d
-	}
+
 	for _, d := range topics {
 		topicData[d.ID] = d
 	}
@@ -92,23 +70,4 @@ func (r *topicResolver) Right() string {
 }
 func (r *topicResolver) HasVoted() *string {
 	return r.d.HasVoted
-}
-
-func (r *Resolver) Droid(args struct{ ID graphql.ID }) (*droidResolver, error) {
-	if d := droidData[args.ID]; d != nil {
-		return &droidResolver{d: d}, nil
-	}
-	return nil, errors.New("This is not the droid you are looking for")
-}
-
-type droidResolver struct {
-	d *droid
-}
-
-func (r *droidResolver) ID() graphql.ID {
-	return r.d.ID
-}
-
-func (r *droidResolver) Name() string {
-	return r.d.Name
 }
