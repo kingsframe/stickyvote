58c77b1d714db609db634cb343636a4992995f4d
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-12-24 16:17:36 -0800

    add query type

--- client/src/containers/TopicListContainer.tsx
+++ client/src/containers/TopicListContainer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useQuery} from '@apollo/react-hooks';
 import gql from 'graphql-tag';
+import {getTopics} from "./__generated__/getTopics";
 
 const GET_TOPICS = gql`
     query getTopics {
@@ -14,11 +15,14 @@ const GET_TOPICS = gql`
 `;
 
 export function TopicListContainer() {
-    const {loading, error, data} = useQuery(GET_TOPICS);
+    const {loading, error, data} = useQuery<getTopics>(GET_TOPICS);
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error :(</p>;
     console.log(data);
+    console.log("vote1:", data?.getTopics[0].hasVoted);
+    console.log("vote2:", data?.getTopics[1].hasVoted);
+    console.log("vote3:", data?.getTopics[2].hasVoted);
     return <div>got data!</div>;
 }
 
