7117af27d3c4bf45a437c29d5cfc756d6744ddb9
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2020-01-26 17:58:30 -0800

    add codegen for types

--- server_ts/src/index.ts
+++ server_ts/src/index.ts
@@ -1,22 +1,27 @@
+import {Choice, Topic} from "./generated/graphql";
+
 const {ApolloServer, gql} = require('apollo-server');
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
 const typeDefs = gql`
-    # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-    # This "Book" type defines the queryable fields for every book in our data source.
-    type Book {
-        title: String
-        author: String
-    }
-
-    # The "Query" type is special: it lists all of the available queries that
-    # clients can execute, along with the return type for each. In this
-    # case, the "books" query returns an array of zero or more Books (defined above).
     type Query {
-        books: [Book]
+        getTopics: [Topic!]!
+        getDemoTopic: Topic!
+    }
+    type Mutation {
+        vote(topic: ID!, choice: Choice!): ID
+    }
+    type Topic {
+        id: ID!
+        left: String!
+        right: String!
+        hasVoted: Choice
+    }
+    enum Choice {
+        LEFT
+        RIGHT
     }
 `;
 
@@ -27,11 +32,8 @@ const resolvers = {
         // launch: (_, {id}, {dataSources}) =>
         //     dataSources.launchAPI.getLaunchById({launchId: id}),
         // me: (_, __, {dataSources}) => dataSources.userAPI.findOrCreateUser()
-        books: () => {
-            return [{
-                title: "im a title222",
-                author: "im a author"
-            }]
+        getDemoTopic: (): Topic => {
+            return {id: "0", left: "Bernie", right: "Trump", hasVoted: Choice.Left}
         }
     }
 };
